dd38f39da47530fecad7eb1ddbab5a74d8f71083
yonghee555 <yonghee5733@gmail.com>
2021-07-26 02:46:21 +0900

    등록/수정/삭제 테스트를 위한 페이지 및 Unit Test 추가

--- safetyvest_server/src/main/java/com/hanium/safetyvest/service/posts/PostsService.java
+++ safetyvest_server/src/main/java/com/hanium/safetyvest/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.hanium.safetyvest.service.posts;
 
 import com.hanium.safetyvest.domain.posts.Posts;
 import com.hanium.safetyvest.domain.posts.PostsRepository;
+import com.hanium.safetyvest.web.dto.PostsListResponseDto;
 import com.hanium.safetyvest.web.dto.PostsResponseDto;
 import com.hanium.safetyvest.web.dto.PostsSaveRequestDto;
 import com.hanium.safetyvest.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,19 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        postsRepository.delete(posts);
     }
 }
